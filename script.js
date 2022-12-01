f7c9f3b4b7d90a76279e248dee1de58bfb4e425e
Elodie Morel <morelelodie037@gmail.com>
2022-12-01 16:17:39 +0000

    DOM Selection

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function main () {
+/**
+ * JS doc comment
+ * @param {string} a -- hello
+ * 
+ * 
+ * 
+ */
+
+function main (a) {
   // d3 code goes here
-  d3.selectAll('p').style('color', 'green')
+  d3.selectAll('.myclass').style('color', 'blue');
+  d3.select('tr').selectAll('td').style('background-color', 'firebrick');
 }
