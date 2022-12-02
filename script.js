7122f0e3ca14487ddc687105b8dd75ffd3ff13b2
EbreadM <morelelodie037@gmail.com>
2022-12-02 12:39:44 +0000

    Method Chaining and Functions of Data

--- script.js
+++ script.js
@@ -8,39 +8,17 @@
 
 function main (a) {
   // d3 code goes here
-  // DOM SELECTION
-  d3.selectAll('.myclass').style('color', 'blue')
-  d3.select('tr').selectAll('td').style('background-color', 'firebrick')
-
-  // DOM MANIPULATION
-  // insert
-  d3.select('div')
-    .insert('p')
-    .text('CIRCUITS CIRCUITS CIRCUITS')
-  // text method
-  d3.select('p')
-    .text('farshad and his circuits')
-  // append method
-  d3.select('body').append('p')
-    .text('3rd para - not really')
-  // insert method
-  d3.select('.bob').insert('p')
-    .text('Here is my new OBSESSION: Cereal').insert('p').text('bob')
-  // remove Method
-  d3.select('p').remove() // removes 'farshad and his circuits'
-  // html method - insert html directly
-  d3.select('#htmlmethod')
-    .insert('p')
-    .text('Thing')
-  d3.select('#htmlmethod').html('<em>This is a new inner html</em>')
-  // attr method
-  d3.select('#summon').attr('class', 'error') // makes error red - look at css idk
-  // property Method
-  d3.select('input').property('checked', true) // if we uesed selectAll then all of the boxes will be ticked.
-  // style Method
-  d3.select('.type').style('color', 'aqua')
-  // classed method
-  d3.select('#summon').classed('error', true) // changes class to error
-  d3.select('#summon').style('color', 'firebrick')
-  // changes style to color: firebrick
+  //chaining method- part of this is the chaining method as you can put them on a new line and they still run.Also doesnt care abt whitespace.
+  /*d3.select('body')
+    .append('p').attr('style', 'color: navy')
+  */
+  //also do this by storing everything into it's own variable (El for element)
+/*var bodyEl = d3.select('body');
+  var para = bodyEl.append('p');
+  para.text('whyyyieeeyeee');
+*/
+  //the above using chaining method
+  d3.select('body')
+    .append('p')
+    .text('i dislike JS icl but DS4E is saving me')
 }
