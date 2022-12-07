9580347eeacc793c99edeb7ba4b97d1bb4bf39b6
Elodie Morel <morelelodie037@gmail.com>
2022-12-07 18:35:54 +0000

    creating svg elements

--- script.js
+++ script.js
@@ -6,78 +6,54 @@
  *
  */
 function main (a) {
-  // var myData = ['Welcome to pain and suffering', 'we are learning about pain', 'this is not fun']
-  /* const myData = ['this', 'is', 'a', 'list', 1, 2, 3, 4, 5]
-  d3.select('body')
-    .selectAll('span')
-    .data(myData)
-    .enter() // Dynamically create placeholders
-    .append('span')
-    .text(function (d, i) {
-      return d + ' ';
-    })
+  const width = 500
+  const height = 500
+  // create SVG element
+  const svg = d3.select('body').append('svg')
+    .attr('width', width)
+    .attr('height', height)
+  // line
+  /*
+  svg.append('line')
+    .attr('x1', 100)
+    .attr('x2', 400)
+    .attr('y1', 40)
+    .attr('y2', 400)
+    .style('stroke', 'navy') */
+  // rectangle
+  /* svg.append('rect')
+    .attr('x', 0)
+    .attr('y', 0)
+    .attr('width', 100)
+    .attr('height', 200)
+    .style('fill', 'blue') */
+  // circle
+  /* svg.append('circle').attr('cx', 250).attr('cy', 50)
+    .attr('r', 60)
+    .attr('fill', 'red') */
+  // ellipse
+  /* svg.append('ellipse').attr('cx', 250).attr('cy', 50)
+    .attr('rx', 150)
+    .attr('ry', 50)
+    .attr('fill', 'red')
   */
-  /* const myData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-  d3.select('body')
-    .selectAll('span')
-    .data(myData)
-    .enter() // Dynamically create placeholders
-    .append('span')
-    .style('color', function (d) {
-      if (d % 3 === 0) {
-        return 'red'
-      } else {
-        return 'blue'
-      }
+  // transformations
+  const g = svg.append('g')
+    .attr('transform', function (d, i) {
+      return 'translate(0,0)'
     })
-    .text(function (d) {
-      return d
-    })
-  */
-  d3.select('body')
-    .select('p')
-    .datum(9000)
-    .text(function (d, i) {
-      return 'His power is over ' + d
-    })
-  /*const myData = ['hello everyone!']
-  d3.select('body')
-    .selectAll('p')
-    .data(myData)
-    .text(function (d) {
-      return d
-    })
-    .exit()
-    .remove()
-  */
-  const mat = [
-    [1, 2, 3, 4],
-    [5, 6, 7, 8],
-    [9, 10, 11, 12],
-    [13, 14, 15, 16]
-  ]
-  // taking and creating a row for each of the rows in our matrix
-  const tr = d3.select('body')
-    .append('table')
-    .selectAll('tr')
-    .data(mat)
-    .enter()
-    .append('tr') // create tr placeholder
-  // making each of the cells inside and filling each of them with the text from the value in the array
-  const td = tr.selectAll('td')
-    .data(function (d) {
-      return d
-    })
-    .enter()
-    .append('td')
-    .style('color', function (d) {
-      if (d % 2 === 0) {
-        return 'red'
-      } else {
-        return 'blue'
-      }
-    })
-    .text(function (d) {
-      return d
-    })
+  const circ = g.append('circle')
+  .attr('cx', 250)
+  .attr('cy', 50)
+  .attr('r', 50)
+  .attr('fill', 'red')
+  .attr('opacity', 0.5);
+
+  g.append('text')
+  .attr('x', 213)
+  .attr('y', 50)
+  .attr('stroke', 'black')
+  .attr('font-family', 'sans-serif')
+  .attr('font-size', '24px')
+  .text('danger')
 }
