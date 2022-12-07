2b34bf9c2a8ba7af001bd0bebf7681cd0a5c9c7e
Elodie Morel <morelelodie037@gmail.com>
2022-12-07 15:13:21 +0000

    data binding examples

--- script.js
+++ script.js
@@ -6,45 +6,78 @@
  *
  */
 function main (a) {
-  /* d3.select('#container')
-    .transition().duration(2000)
-    .style('background-color','red')
-    .transition().duration(2000)
-    .style('background-color','yellow')
+  // var myData = ['Welcome to pain and suffering', 'we are learning about pain', 'this is not fun']
+  /* const myData = ['this', 'is', 'a', 'list', 1, 2, 3, 4, 5]
+  d3.select('body')
+    .selectAll('span')
+    .data(myData)
+    .enter() // Dynamically create placeholders
+    .append('span')
+    .text(function (d, i) {
+      return d + ' ';
+    })
   */
-  // same thing just with variables
-  /* var t = d3.transition().duration(2000)
-  d3.select('#container')
-    .transition(t)
-    .style('background-color', 'yellow')
+  /* const myData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  d3.select('body')
+    .selectAll('span')
+    .data(myData)
+    .enter() // Dynamically create placeholders
+    .append('span')
+    .style('color', function (d) {
+      if (d % 3 === 0) {
+        return 'red'
+      } else {
+        return 'blue'
+      }
+    })
+    .text(function (d) {
+      return d
+    })
   */
-  const svg = d3.select('body')
-    .append('svg')
-    .attr('width', 500)
-    .attr('height', 500)
-  const bar1 = svg.append('rect')
-    .attr('fill', 'navy')
-    .attr('x', 100)
-    .attr('y', 20)
-    .attr('height', 20)
-    .attr('width', 10)
-  const bar2 = svg.append('rect')
-    .attr('fill', 'firebrick')
-    .attr('x', 120)
-    .attr('y', 20)
-    .attr('height', 20)
-    .attr('width', 10)
-  update();
-
-  function update() {
-    bar1.transition()
-      .ease(d3.easeLinear)
-      .duration(2000)
-      .attr('height', 100)
-    bar2.transition()
-      .ease(d3.easeLinear)
-      .duration(2000)
-      .delay(2000)
-      .attr('height', 100);
-  }
+  d3.select('body')
+    .select('p')
+    .datum(9000)
+    .text(function (d, i) {
+      return 'His power is over ' + d
+    })
+  /*const myData = ['hello everyone!']
+  d3.select('body')
+    .selectAll('p')
+    .data(myData)
+    .text(function (d) {
+      return d
+    })
+    .exit()
+    .remove()
+  */
+  const mat = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]
+  ]
+  // taking and creating a row for each of the rows in our matrix
+  const tr = d3.select('body')
+    .append('table')
+    .selectAll('tr')
+    .data(mat)
+    .enter()
+    .append('tr') // create tr placeholder
+  // making each of the cells inside and filling each of them with the text from the value in the array
+  const td = tr.selectAll('td')
+    .data(function (d) {
+      return d
+    })
+    .enter()
+    .append('td')
+    .style('color', function (d) {
+      if (d % 2 === 0) {
+        return 'red'
+      } else {
+        return 'blue'
+      }
+    })
+    .text(function (d) {
+      return d
+    })
 }
